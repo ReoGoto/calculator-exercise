aadfc4ba1d53556da5639b79e92c0767f6559512
Reo Goto <reo5100go@gmail.com>
2020-03-13 17:22:10 -0500

    make it better

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-const WebSocket = require('ws')
-
-const wss = new WebSocket.Server({ port: 8080 })
-
-wss.on('connection', ws => {
-  ws.on('message', message => {
-    console.log(`Received message => ${message}`)
-  })
-  ws.send('ho!')
-})
-
-
-
 
 // var WebSocketServer = require('ws').Server
 //     , http = require('http')
@@ -51,29 +38,29 @@ wss.on('connection', ws => {
 
 
 
-// const ws = new require('ws');
-// const wss = new ws.Server({noServer: true});
+const ws = new require('ws');
+const wss = new ws.Server({noServer: true});
 
-// const clients = new Set();
+const clients = new Set();
 
-// http.createServer((req, res) => {
-//   // here we only handle websocket connections
-//   // in real project we'd have some other code here to handle non-websocket requests
-//   wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
-// });
+http.createServer((req, res) => {
+  // here we only handle websocket connections
+  // in real project we'd have some other code here to handle non-websocket requests
+  wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
+});
 
-// function onSocketConnect(ws) {
-//   clients.add(ws);
+function onSocketConnect(ws) {
+  clients.add(ws);
 
-//   ws.on('message', function(message) {
-//     message = message.slice(0, 50); // max message length will be 50
+  ws.on('message', function(message) {
+    message = message.slice(0, 50); // max message length will be 50
 
-//     for(let client of clients) {
-//       client.send(message);
-//     }
-//   });
+    for(let client of clients) {
+      client.send(message);
+    }
+  });
 
-//   ws.on('close', function() {
-//     clients.delete(ws);
-//   });
-// }
+  ws.on('close', function() {
+    clients.delete(ws);
+  });
+}
